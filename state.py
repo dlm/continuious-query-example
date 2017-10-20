2781db2e6fed9acb6b4abe3122f239e4e9df5300
David Millman <dave@cs.unc.edu>
2017-10-20 17:48:37 -0600

    Use use general rabbit in state

--- state.py
+++ state.py
@@ -1,6 +1,6 @@
 import rabbit
 
-(connection, channel) = rabbit.RabbitConnection.setup()
+(connection, channel, queue_name, exchange_name) = rabbit.RabbitConnection.setup()
 
 state = dict()
 
@@ -8,9 +8,12 @@ def callback(ch, method, properties, body):
     [key, val] = str(body).split(':')
     print(" [x] Received %s %s" % (key, val))
     state[key] = val
+    channel.basic_publish(exchange=exchange_name,
+                          routing_key='',
+                          body=val)
 
 channel.basic_consume(callback,
-                      queue='hello',
+                      queue=queue_name,
                       no_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
