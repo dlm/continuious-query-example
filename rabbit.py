1eb68d0dd68652db5d68d1e7ded556c50eab93f1
David Millman <dave@cs.unc.edu>
2017-10-20 17:34:14 -0600

    Set queue and exchange names

--- rabbit.py
+++ rabbit.py
@@ -3,11 +3,11 @@ import pika
 class RabbitConnection(object):
 
     @classmethod
-    def setup(cls):
+    def setup(cls, queue_name='updates', exchange_name='logs'):
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         channel = connection.channel()
-        channel.queue_declare(queue='hello')
-        return (connection, channel)
-
-
+        queue_result = channel.queue_declare(queue=queue_name)
+        exchange_result = channel.exchange_declare(exchange=exchange_name,
+                                                   exchange_type='fanout')
+        return (connection, channel, queue_name, exchange_name)
 
